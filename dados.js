6c8d4329674f01fc0c49b8eb5295401165da1d62
wagnerods <aolimpiodasilva@gmail.com>
2024-09-07 18:48:44 -0300

    chore: Criando base dados 'dados.js'

--- dados.js
+++ dados.js
@@ -1,1 +1,29 @@
-let dados = "PETR4";
+let dados = [
+    petr4 = {
+        empresa: "Petrobras",
+        nome: "Petrobras",
+        preco: 28.10,
+        setor: "Petróleo",
+        lucro: 10.5,
+        dividendos: 5.5,
+        link: "https://www.petrobras.com.br/pt/"
+    },
+    vale3 = {
+        empresa: "Vale",
+        nome: "Vale",
+        preco: 54.10,
+        setor: "Mineração",
+        lucro: 15.5,
+        dividendos: 7.5,
+        link: "https://www.vale.com/"
+    },
+    magalu = {
+        empresa: "Magazine Luiza",
+        nome: "Magalu",
+        preco: 25.10,
+        setor: "Varejo",
+        lucro: 5.5,
+        dividendos: 2.5,
+        link: "https://www.magazineluiza.com.br/"
+    }
+];  // End of dados
